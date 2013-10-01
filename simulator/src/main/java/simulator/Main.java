a362c2714c46253fadcf28a5a64b36ed945f97af
JMarginier <j.marginier@gmail.com>
2013-10-01 10:58:43 +0200

    Init du projet. -> mise en place du systeme de traces

---
+++ simulator/src/main/java/simulator/Main.java
@@ -0,0 +1,15 @@
+package simulator;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+	private static Logger log = LoggerFactory.getLogger(Main.class);
+	
+	public static void main(String[] args) {
+		log.info("Coucou le simulateur de flux monetique");
+	}
+
+}

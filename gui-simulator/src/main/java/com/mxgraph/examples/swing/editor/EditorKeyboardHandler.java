773f2ea706db125424937adbec0bb5e3bbeb8172
Floww <floww14@gmail.com>
2013-12-28 17:01:06 +0100

    Disable Undo/Redo.

--- gui-simulator/src/main/java/com/mxgraph/examples/swing/editor/EditorKeyboardHandler.java
+++ gui-simulator/src/main/java/com/mxgraph/examples/swing/editor/EditorKeyboardHandler.java
@@ -11,44 +11,40 @@ import javax.swing.KeyStroke;
 
 import com.mxgraph.swing.mxGraphComponent;
 import com.mxgraph.swing.handler.mxKeyboardHandler;
-import com.mxgraph.swing.util.mxGraphActions;
 
 /**
  * @author Administrator
  * 
  */
-public class EditorKeyboardHandler extends mxKeyboardHandler
-{
+public class EditorKeyboardHandler extends mxKeyboardHandler {
 
 	/**
 	 * 
 	 * @param graphComponent
 	 */
-	public EditorKeyboardHandler(mxGraphComponent graphComponent)
-	{
+	public EditorKeyboardHandler(mxGraphComponent graphComponent) {
 		super(graphComponent);
 	}
 
 	/**
 	 * Return JTree's input map.
 	 */
-	protected InputMap getInputMap(int condition)
-	{
+	protected InputMap getInputMap(int condition) {
 		InputMap map = super.getInputMap(condition);
 
-		if (condition == JComponent.WHEN_FOCUSED && map != null)
-		{
+		if (condition == JComponent.WHEN_FOCUSED && map != null) {
 			map.put(KeyStroke.getKeyStroke("control S"), "save");
 			map.put(KeyStroke.getKeyStroke("control shift S"), "saveAs");
 			map.put(KeyStroke.getKeyStroke("control N"), "new");
 			map.put(KeyStroke.getKeyStroke("control O"), "open");
 
-			map.put(KeyStroke.getKeyStroke("control Z"), "undo");
-			map.put(KeyStroke.getKeyStroke("control Y"), "redo");
-			map
-					.put(KeyStroke.getKeyStroke("control shift V"),
-							"selectVertices");
-			map.put(KeyStroke.getKeyStroke("control shift E"), "selectEdges");
+			// map.put(KeyStroke.getKeyStroke("control Z"), "undo");
+			// map.put(KeyStroke.getKeyStroke("control Y"), "redo");
+			// map
+			// .put(KeyStroke.getKeyStroke("control shift V"),
+			// "selectVertices");
+			// map.put(KeyStroke.getKeyStroke("control shift E"),
+			// "selectEdges");
 		}
 
 		return map;
@@ -57,18 +53,17 @@ public class EditorKeyboardHandler extends mxKeyboardHandler
 	/**
 	 * Return the mapping between JTree's input map and JGraph's actions.
 	 */
-	protected ActionMap createActionMap()
-	{
+	protected ActionMap createActionMap() {
 		ActionMap map = super.createActionMap();
 
 		map.put("save", new EditorActions.SaveAction(false));
 		map.put("saveAs", new EditorActions.SaveAction(true));
 		map.put("new", new EditorActions.NewAction());
 		map.put("open", new EditorActions.OpenAction());
-		map.put("undo", new EditorActions.HistoryAction(true));
-		map.put("redo", new EditorActions.HistoryAction(false));
-		map.put("selectVertices", mxGraphActions.getSelectVerticesAction());
-		map.put("selectEdges", mxGraphActions.getSelectEdgesAction());
+		// map.put("undo", new EditorActions.HistoryAction(true));
+		// map.put("redo", new EditorActions.HistoryAction(false));
+		// map.put("selectVertices", mxGraphActions.getSelectVerticesAction());
+		// map.put("selectEdges", mxGraphActions.getSelectEdgesAction());
 
 		return map;
 	}
